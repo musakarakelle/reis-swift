b623691bd1c2a2ba0aaa7949490d8c3bb61c6f3f
REIS <reis@reiss-macbook-pro.local>
2016-12-22 11:17:29 +0300

    Initial Commit

---
+++ swif2Tests/swif2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  swif2Tests.swift
+//  swif2Tests
+//
+//  Created by REIS on 12/22/16.
+//  Copyright © 2016 REIS. All rights reserved.
+//
+
+import XCTest
+@testable import swif2
+
+class swif2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
